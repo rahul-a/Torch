7086a78f5c280afb502d2f166cfdf5b88342131c
Rahul Sharma <rahul.taicho@gmail.com>
2019-11-01 16:01:14 +0530

    Adds circular view target to Spotlight

--- app/src/main/java/com/taicho/sample/MainActivity.kt
+++ app/src/main/java/com/taicho/sample/MainActivity.kt
@@ -1,12 +1,11 @@
 package com.taicho.sample
 
+import android.annotation.SuppressLint
 import android.content.Context
-import android.graphics.Point
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.Button
-import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
 import com.taicho.spotlight.Spotlight
 import com.taicho.spotlight.target.Circle
@@ -18,26 +17,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val contentView = FrameLayout(this)
-        setContentView(contentView)
+        setContentView(R.layout.activity_main)
 
-        contentView.post {
-            Spotlight()
-                .describe(DummyDescription(this))
-                .target(Circle(getCenter(contentView), TARGET_RADIUS))
-                .focus(this)
-        }
-    }
-
-    private fun getCenter(view: View): Point {
-        val x = view.measuredWidth.toFloat() / 2
-        val y = view.measuredHeight.toFloat() / 2
-
-        return Point(x.toInt(), y.toInt())
+        Spotlight()
+            .describe(DummyDescription(this))
+            .target(Circle(findViewById(R.id.btn1), TARGET_RADIUS))
+            .focus(this)
     }
 
     private class DummyDescription(private val context: Context) : Spotlight.Description() {
 
+        @SuppressLint("InflateParams")
         override fun getView(): View {
             val view = LayoutInflater.from(context).inflate(R.layout.description_sample, null)
             view.findViewById<Button>(R.id.btnDismiss).setOnClickListener { dismiss() }
